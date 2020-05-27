5756d793beb4cf35a7a17865e74696a43b9cdc17
Abdullah Esmail <abdullah.esmail@gmail.com>
2020-05-27 07:53:08 +0300

    check phoenix version.

--- lib/kaffy/utils.ex
+++ lib/kaffy/utils.ex
@@ -13,6 +13,21 @@ defmodule Kaffy.Utils do
     env(:ecto_repo)
   end
 
+  def get_version_of(package) do
+    {:ok, version} = :application.get_key(package, :vsn)
+    version
+  end
+
+  def phoenix_1_4?() do
+    version = get_version_of(:phoenix)
+    String.starts_with?(version, "1.4.")
+  end
+
+  def phoenix_1_5?() do
+    version = get_version_of(:phoenix)
+    String.starts_with?(version, "1.5.")
+  end
+
   def router do
     Module.concat(env(:router), Helpers)
   end
