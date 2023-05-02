cdb607e39b1d657411697b48d8e8f8c91795df5b
Marcel Lindig <ml@nullpilot.de>
2023-05-02 16:36:09 +0200

    fix resource redirects

--- lib/kaffy_web/controllers/resource_controller.ex
+++ lib/kaffy_web/controllers/resource_controller.ex
@@ -394,7 +394,8 @@ defmodule KaffyWeb.ResourceController do
   end
 
   defp redirect_to_resource(conn, context, resource, entry) do
-    id = Kaffy.ResourceAdmin.serialize_id(resource, entry)
+    my_resource = Kaffy.Utils.get_resource(conn, context, resource)
+    id = Kaffy.ResourceAdmin.serialize_id(my_resource, entry)
 
     redirect(conn,
       to:
