861d810af4688be6a823777c8cd26afe91d134ff
Matteo Gheri <57362031+matteo-gheri-pocketworks@users.noreply.github.com>
2024-08-29 08:59:39 +0100

    accept value_fn for {:array, _} field

--- lib/kaffy/resource_form.ex
+++ lib/kaffy/resource_form.ex
@@ -243,7 +243,14 @@ defmodule Kaffy.ResourceForm do
         case !is_nil(options[:values_fn]) && is_function(options[:values_fn], 2) do
           true ->
             values = options[:values_fn].(data, conn)
-            value = Map.get(data, field, nil)
+
+            value =
+              if is_function(options[:value_fn], 1) do
+                options[:value_fn].(data)
+              else
+                Map.get(data, field, nil)
+              end
+
             multiple_select(form, field, values, [value: value] ++ opts)
 
           false ->
